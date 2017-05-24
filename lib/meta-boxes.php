4ddfd286bc6493778a862880986bad7bc3ebcaa7
Patrick Best <patrickbest@patrickbest.com>
2017-05-24 22:53:58 +0100

    update metabox declaration

--- lib/meta-boxes.php
+++ lib/meta-boxes.php
@@ -1,69 +1,70 @@
 <?php
-add_filter( 'cmb_meta_boxes', 'cmb_sample_metaboxes' );
+/**
+ * Hook in and add metaboxes. Can only happen on the 'cmb2_init' hook.
+ */
+add_action( 'cmb2_init', 'igv_cmb_metaboxes' );
+function igv_cmb_metaboxes() {
 
-function cmb_sample_metaboxes( array $meta_boxes ) {
-
+	// Start with an underscore to hide fields from custom fields list
 	$prefix = '_cmb_';
 
-	$meta_boxes[] = array(
-		'id' => 'about_metabox',
-		'title' => 'About meta',
-		'pages' => array('page'),
-		'show_on' => array( 'key' => 'id', 'value' => array( 50 ) ),
-		'context' => 'normal',
-		'priority' => 'high',
-		'show_names' => true,
-		'fields' => array(
-			array(
-				'name' => 'Short about',
-				'desc' => 'Viewable on the frontpage',
-				'id'   => $prefix . 'short_about',
-				'type' => 'wysiwyg',
-			)
-		),
-	);
+	/**
+	 * Metaboxes declarations here
+   * Reference: https://github.com/WebDevStudios/CMB2/blob/master/example-functions.php
+	 */
 
-	$meta_boxes[] = array(
-		'id'         => 'postype_metabox',
-		'title'      => 'Project Settings',
-		'pages'      => array( 'project', ),
-		'context'    => 'normal',
-		'priority'   => 'high',
-		'show_names' => true,
-		'fields'     => array(
-			array(
-				'name' => 'Editioning number',
-				'desc' => '...',
-				'id'   => $prefix . 'editions',
-				'type' => 'text',
-			),
-			array(
-				'name' => 'Indent',
-				'desc' => 'a number up to 530',
-				'id'   => $prefix . 'indent',
-				'type' => 'text',
-			),
-			array(
-				'name' => 'Color',
-				'desc' => 'text color as css code',
-				'id'   => $prefix . 'color',
-				'type' => 'text',
-			),
-			array(
-				'name' => 'Logo SVG',
-				'desc' => '...',
-				'id'   => $prefix . 'logo_svg',
-				'type' => 'file',
-			),
-			array(
-				'name' => 'Fallback logo PNG',
-				'desc' => 'for older browsers that do not support',
-				'id'   => $prefix . 'logo_png',
-				'type' => 'file',
-			)
-		),
-	);
+  $about_meta = new_cmb2_box( array(
+		'id'            => $prefix . 'about',
+		'title'         => esc_html__( 'About Metabox', 'cmb2' ),
+		'object_types'  => array( 'page', ), // Post type
+	) );
 
-	return $meta_boxes;
+	$about_meta->add_field( array(
+		'name'       => esc_html__( 'Short about', 'cmb2' ),
+		'desc'       => esc_html__( 'viewable on the frontpage', 'cmb2' ),
+		'id'         => $prefix . 'short_about',
+		'type'       => 'wysiwyg',
+	) );
+
+  // PROJECT META
+
+  $project_meta = new_cmb2_box( array(
+		'id'            => $prefix . 'project',
+		'title'         => esc_html__( 'Project Metabox', 'cmb2' ),
+		'object_types'  => array( 'project', ), // Post type
+	) );
+
+	$project_meta->add_field( array(
+		'name'       => esc_html__( 'Editioning number', 'cmb2' ),
+		'id'         => $prefix . 'editions',
+		'type'       => 'text',
+	) );
+
+	$project_meta->add_field( array(
+		'name'       => esc_html__( 'Indent', 'cmb2' ),
+		'desc'       => esc_html__( 'a number up to 530', 'cmb2' ),
+		'id'         => $prefix . 'indent',
+		'type'       => 'text',
+	) );
+
+	$project_meta->add_field( array(
+		'name'       => esc_html__( 'Color', 'cmb2' ),
+		'desc'       => esc_html__( 'text color as css code', 'cmb2' ),
+		'id'         => $prefix . 'color',
+		'type'       => 'text',
+	) );
+
+	$project_meta->add_field( array(
+		'name'       => esc_html__( 'Logo SVG', 'cmb2' ),
+		'id'         => $prefix . 'logo_svg',
+		'type'       => 'file',
+	) );
+
+	$project_meta->add_field( array(
+		'name'       => esc_html__( 'Fallback logo PNG', 'cmb2' ),
+		'id'         => $prefix . 'logo_png',
+		'type'       => 'file',
+	) );
+
 }
 ?>
