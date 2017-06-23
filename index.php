25138a9caed2491eae5a4ff482ba8ff11472f2b5
Patrick Best <patrickbest@patrickbest.com>
2017-06-23 20:42:13 +0100

    add mailchimp form

--- index.php
+++ index.php
@@ -52,6 +52,36 @@
 
 			<div class="page-content content-triggered">
 				<?php pageContent('about'); ?>
+
+				<!-- Begin MailChimp Signup Form -->
+        <div id="mc_embed_signup">
+          <form action="//rotate-editions.us13.list-manage.com/subscribe/post?u=ae07f925ac9375ac81960fdd5&amp;id=20de9646be" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+              <h3>Subscribe to our mailing list</h3>
+              <div class="mc-field-group">
+              	<label for="mce-EMAIL">Email Address*</label>
+              	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+              </div>
+              <div class="mc-field-group">
+              	<label for="mce-FNAME">First Name </label>
+              	<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+              </div>
+              <div class="mc-field-group">
+              	<label for="mce-LNAME">Last Name </label>
+              	<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+              </div>
+            	<div id="mce-responses" class="clear">
+            		<div class="response" id="mce-error-response" style="display:none"></div>
+            		<div class="response" id="mce-success-response" style="display:none"></div>
+            	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+              <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ae07f925ac9375ac81960fdd5_20de9646be" tabindex="-1" value=""></div>
+              <div class="mc-field-group cf">
+                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+              </div>
+            </div>
+          </form>
+        </div>
+        <!--End mc_embed_signup-->
 			</div>
 		</div>
 
