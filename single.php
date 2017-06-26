58c8e7b83c8b6bf8bc278f0a455df64ab4d6d7c3
Patrick Best <patrickbest@patrickbest.com>
2017-06-26 19:43:11 +0100

    tabs to spaces

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php
-	$redirect = home_url('#!/project/' . $post->post_name);
-	header('HTTP/1.1 301 Moved Permanently');
-	header ('Location: ' . $redirect);
-	exit;
+  $redirect = home_url('#!/project/' . $post->post_name);
+  header('HTTP/1.1 301 Moved Permanently');
+  header ('Location: ' . $redirect);
+  exit;
 ?>
